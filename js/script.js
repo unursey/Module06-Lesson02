5c4f5b9d401dd5dd1f2abc7f03caf238e749f22c
Anna O <usune@mail.ru>
2022-07-20 00:07:07 +0300

    mm

--- js/script.js
+++ js/script.js
@@ -1,30 +1,46 @@
-'use strict';
+"use strict";
 
-document.body.insertAdjacentHTML('afterbegin', `
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `
   <label class="form-group m-5">
     <input type="text" class="form-control" 
     name="task" placeholder="Введите текст">
   </label>
 
   <p class="text m-3">Здесь должен появиться Ваш текст.</p>
-  `,
+  `
 );
 
-const p = document.querySelector('.text');
-const input = document.querySelector('.form-control');
 
-input.focus();
 
-input.addEventListener('input', () => {
-  if (input.value.trim() !== '') {
-    setTimeout(() => {
+// input.addEventListener('input', () => {
+//   if (input.value.trim() !== '') {
+//     setTimeout(() => {
+//       p.textContent = input.value;
+//       clearTimeout();
+//     }, 300);
+//   } else {
+//     input.value = '';
+//     input.focus();
+//   }
+// });
+
+const foo = () => {
+  const p = document.querySelector(".text");
+  const input = document.querySelector(".form-control");
+
+  input.focus();
+
+  let t;
+
+  input.addEventListener('input', () => {
+    clearTimeout(t);
+
+    t = setTimeout(() => {
       p.textContent = input.value;
-      clearTimeout();
     }, 300);
-  } else {
-    input.value = '';
-    input.focus();
-  }
-});
+  });
+};
 
-
+foo();
